e6c14a976b5d83cf108580565dc31087c1db4dd2
ARAPBAEV777 <arapbaevnurdan0225@gmail.com>
2025-10-22 10:48:22 +0600

    rfw

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-print("Hello Nurdan")
+import random
+from itertools import count
 
+num1 =  int(input(''))
+num2 =  int(input(''))
 
+word = random.randint(num1,num2)
 
+count = 0
 
-
-git
+while True:
+    number = int(input())
+    if number == word:
+        print()
